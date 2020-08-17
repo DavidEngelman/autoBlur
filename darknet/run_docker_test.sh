76c61262d4d0b02f53ba0475054aa25e1027f8bb
David Engelman <David.Engelman@ulb.ac.be>
2020-08-17 14:31:32 +0200

    update scripts

---
+++ darknet/run_docker_test.sh
@@ -0,0 +1,2 @@
+sudo docker run -p 80:90/tcp --network="host" --gpus all -it --rm -v $PWD:/workspace -w /workspace --name ab autoblur-docker:latest darknet detector train build/darknet/x64/data/head.data cfg/yolov4-tiny-head-test.cfg build/darknet/x64/yolov4-tiny.conv.29 -dont_show -mjpeg_port 8090 -map
+
--- run_docker.sh
+++
@@ -1,2 +0,0 @@
-sudo docker run --gpus all -it --rm -v $PWD:/workspace -w /workspace --name ab autoblur-docker:latest darknet detector train build/darknet/x64/data/head.data cfg/yolov4-tiny-head-test.cfg build/darknet/x64/yolov4-tiny.conv.29 -dont_show
-
